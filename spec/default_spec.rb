bcb371d56b21ea44cde74af61f63a30d97f79ee0
Doc Walker <doc.walker@jameshardie.com>
2014-01-08 07:09:37 -0600

    Convert to rspec :expect syntax

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'sysctl::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'sysctl::default' }
-  
-  it 'should test ruby_block' do
+
+  it 'tests ruby_block' do
     pending 'TODO: figure out how to test ruby_block'
   end # it
 
