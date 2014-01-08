01414f027fc76dd39daee217ab8cfaeebb2cee5b
Doc Walker <doc.walker@jameshardie.com>
2014-01-08 07:10:18 -0600

    Streamline rspec let blocks

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'sysctl::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'sysctl::default' }
+  let(:chef_run) { ChefSpec::ChefRunner.new.converge(described_recipe) }
 
   it 'tests ruby_block' do
     pending 'TODO: figure out how to test ruby_block'
